d36e15a33a51e64e53d3758fe6ad9d84e0689542
Matteo-Artuso <10610150@polimi.it>
2024-05-31 16:06:03 +0200

    personal data page

--- lib/screens/personal_data_screen.dart
+++ lib/screens/personal_data_screen.dart
@@ -16,60 +16,64 @@ class _PersonalDataScreenState extends State<PersonalDataScreen> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     double width = SizeConfig.screenW!;
-    double heigh = SizeConfig.screenH!;
 
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: appBar(width),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-            horizontal: width * 0.1, vertical: heigh * 0.02),
+      body: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40.0,
+          vertical: 40.0,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 15.0, vertical: 15.0),
-                  child: CircleAvatar(
-                    radius: width * 0.1,
-                    backgroundColor: Colors.amber,
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "${user.getString('firstname')!} ${user.getString('lastname')!}",
-                      style: TextStyle(
-                          fontFamily: 'Bebas',
-                          fontSize: width * 0.07,
-                          height: 1),
-                    ),
-                    Text(user.getString('email')!),
-                  ],
-                )
-              ],
+            const SizedBox(height: 10.0),
+            userInfo(width),
+            const SizedBox(height: 30.0),
+            CustomCard(
+              title: 'First name:',
+              content: user.getString('firstname')!,
             ),
+            const SizedBox(height: 30.0),
             CustomCard(
-                icon: Icons.person, text: 'Personal data', onPressed: () => {}),
+              title: 'Last name:',
+              content: user.getString('lastname')!,
+            ),
+            const SizedBox(height: 30.0),
             CustomCard(
-                icon: Icons.notifications,
-                text: 'Notification',
-                onPressed: () => {}),
+              title: 'Birthdate:',
+              content: user
+                  .getString('birthdate')!
+                  .substring(0, user.getString('birthdate')!.indexOf('T')),
+            ),
+            const SizedBox(height: 30.0),
             CustomCard(
-                icon: Icons.chat_bubble,
-                text: 'Assistance',
-                onPressed: () => {}),
-            CustomCard(
-                icon: Icons.question_mark, text: 'FAQ', onPressed: () => {}),
-            CustomCard(icon: Icons.book, text: 'Guide', onPressed: () => {}),
-            CustomCard(
-                icon: Icons.lightbulb, text: 'Hints', onPressed: () => {}),
-            CustomCard(
-                icon: Icons.translate, text: 'Language', onPressed: () => {}),
+              title: 'Email:',
+              content: user.getString('email')!,
+            ),
+            const SizedBox(height: 50.0),
+            OutlinedButton(
+              style: const ButtonStyle(
+                  minimumSize: MaterialStatePropertyAll(Size(30, 50))),
+              onPressed: () => {},
+              child: const Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 8,
+                    child:
+                        Text('Change password', style: TextStyle(fontSize: 16)),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Icon(Icons.arrow_forward_rounded),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -78,40 +82,46 @@ class _PersonalDataScreenState extends State<PersonalDataScreen> {
 }
 
 class CustomCard extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  final dynamic onPressed;
+  final String title;
+  final String content;
 
   const CustomCard({
-    required this.icon,
-    required this.text,
-    required this.onPressed,
+    required this.title,
+    required this.content,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-        style: const ButtonStyle(
-            minimumSize: MaterialStatePropertyAll(Size(30, 50))),
-        onPressed: onPressed,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              flex: 3,
-              child: Icon(icon),
-            ),
-            Expanded(
-              flex: 8,
-              child: Text(text, style: const TextStyle(fontSize: 16)),
-            ),
-            const Expanded(
-              flex: 2,
-              child: Icon(Icons.arrow_forward_rounded),
-            )
-          ],
-        ));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: kLabelStyle,
+        ),
+        const SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 184, 216, 255),
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 6.0,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          height: 60.0,
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+            child: Text(content),
+          ),
+        ),
+      ],
+    );
   }
 }
