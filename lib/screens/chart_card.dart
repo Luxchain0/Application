9ab562b390c959aca8a04ca90fb95580f98d8559
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-07-25 17:59:09 +0200

    fix: some fixs to charts

--- lib/screens/chart_card.dart
+++ lib/screens/chart_card.dart
@@ -4,9 +4,10 @@ import 'package:intl/intl.dart'; // Per la formattazione delle date
 import 'package:lux_chain/utilities/api_calls.dart';
 import 'package:lux_chain/utilities/api_models.dart';
 import 'package:lux_chain/utilities/size_config.dart';
+import 'package:lux_chain/utilities/utils.dart';
 
 class ChartCard extends StatefulWidget {
-  final int watchId; // Aggiungi qui l'ID dell'orologio
+  final int watchId;
   final bool isShowingMainData;
 
   const ChartCard({
@@ -20,11 +21,11 @@ class ChartCard extends StatefulWidget {
 }
 
 class _ChartCardState extends State<ChartCard> {
-  Set<String> _selected = {'min'};
+  String _selected = 'day';
   List<Candle> futureData = [];
-  final List<FlSpot> _listDots1 = [];
-  final List<FlSpot> _listDots2 = [];
-  List<DateTime> _dates = [];
+  final List<FlSpot> _listDotsMax = [];
+  final List<FlSpot> _listDotsMin = [];
+  final List<DateTime> _dates = [];
 
   double _delta = 0;
   double _min = 0;
@@ -34,13 +35,13 @@ class _ChartCardState extends State<ChartCard> {
   @override
   void initState() {
     super.initState();
-    _initializeData(_selected.first);
+    _initializeData(_selected);
   }
 
-  void updateSelected(Set<String> newSelection) {
+  void updateSelected(String newSelection) {
     setState(() {
       _selected = newSelection;
-      _initializeData(_selected.first);
+      _initializeData(_selected);
     });
   }
 
@@ -50,8 +51,8 @@ class _ChartCardState extends State<ChartCard> {
     double min = double.infinity;
     double max = double.negativeInfinity;
 
-    _listDots1.clear();
-    _listDots2.clear();
+    _listDotsMax.clear();
+    _listDotsMin.clear();
     _dates.clear();
     _contatore = 0;
 
@@ -59,17 +60,11 @@ class _ChartCardState extends State<ChartCard> {
       if (candle.max > max) max = candle.max;
       if (candle.min < min) min = candle.min;
 
-      _listDots1.add(FlSpot(_contatore, candle.max));
-      _listDots2.add(FlSpot(_contatore, candle.min));
-      _dates.add(DateTime.parse(candle.date)); // Parse the ISO 8601 date
+      _listDotsMax.add(FlSpot(_contatore, candle.max));
+      _listDotsMin.add(FlSpot(_contatore, candle.min));
+      _dates.add(DateTime.parse(candle.date));
       _contatore += 1;
     }
-
-    double fractionMax = max % 100;
-    max = max + (100 - fractionMax);
-
-    double fractionMin = min % 100;
-    min = min - fractionMin;
 
     double delta = max - min;
 
@@ -87,114 +82,127 @@ class _ChartCardState extends State<ChartCard> {
     final double height = SizeConfig.screenH!;
     final double width = SizeConfig.screenW!;
 
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: width * 0.07),
-      child: Column(
-        children: [
-          SizedBox(height: height * 0.05),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 30),
-            constraints: BoxConstraints(maxWidth: width * 0.9, maxHeight: height * 0.3),
-            child: LineChart(
-              sampleData,
-              duration: const Duration(milliseconds: 250),
+    return Column(
+      children: [
+        SizedBox(height: height * 0.05),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 30),
+          constraints:
+              BoxConstraints(maxWidth: width * 0.9, maxHeight: height * 0.3),
+          child: LineChart(
+            sampleData,
+            duration: const Duration(milliseconds: 250),
+          ),
+        ),
+        SegmentedButton(
+          segments: const <ButtonSegment<String>>[
+            // ButtonSegment<String>(
+            //   value: 'min',
+            //   label: Text('Min'),
+            // ),
+            ButtonSegment<String>(
+              value: 'hour',
+              label: Text('Hour'),
             ),
-          ),
-          SegmentedButton(
-            segments: const <ButtonSegment<String>>[
-              ButtonSegment<String>(
-                value: 'min',
-                label: Text('Min'),
-              ),
-              ButtonSegment<String>(
-                value: 'hour',
-                label: Text('Hour'),
-              ),
-              ButtonSegment<String>(
-                value: 'day',
-                label: Text('Day'),
-              ),
-            ],
-            selected: _selected,
-            onSelectionChanged: updateSelected,
-          ),
-          SizedBox(height: height * 0.03),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.width * 0.02),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomBoxForMeasure(
-                  measure: 'Maximum sold price',
-                  screenWidth: width,
-                  color: Colors.redAccent,
-                ),
-                SizedBox(width: width * 0.08),
-                CustomBoxForMeasure(
-                  measure: 'Minimum sold price',
-                  screenWidth: width,
-                  color: Colors.green,
-                ),
-              ],
+            ButtonSegment<String>(
+              value: 'day',
+              label: Text('Day'),
             ),
-          ),
-        ],
-      ),
+          ],
+          selected: {_selected},
+          onSelectionChanged: (newSelection) =>
+              updateSelected(newSelection.first),
+        ),
+        SizedBox(height: height * 0.03),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomBoxForMeasure(
+              measure: 'Maximum sold price',
+              screenWidth: width,
+              color: Colors.green,
+            ),
+            CustomBoxForMeasure(
+              measure: 'Minimum sold price',
+              screenWidth: width,
+              color: Colors.redAccent,
+            ),
+          ],
+        ),
+      ],
     );
   }
 
   LineChartData get sampleData => LineChartData(
-    lineTouchData: lineTouchData1,
-    gridData: gridData,
-    titlesData: titlesData1,
-    borderData: borderData,
-    lineBarsData: lineBarsData1,
-    minX: 0,
-    maxX: _contatore,
-    maxY: _max,
-    minY: _min,
-  );
+        lineTouchData: lineTouchData1,
+        gridData: gridData,
+        titlesData: titlesData1,
+        borderData: borderData,
+        lineBarsData: lineBarsData1,
+        minX: 0,
+        maxX: _contatore,
+        maxY: _max + (_max - _min) * 0.1,
+        minY: _min - (_max - _min) * 0.1 > 0 ? _min - (_max - _min) * 0.1 : 0,
+      );
 
   LineTouchData get lineTouchData1 => LineTouchData(
-    handleBuiltInTouches: true,
-    touchTooltipData: LineTouchTooltipData(
-      getTooltipColor: (touchedSpot) => Colors.black45.withOpacity(0.8),
-      tooltipRoundedRadius: 1,
-    ),
-  );
+        handleBuiltInTouches: true,
+        touchTooltipData: LineTouchTooltipData(
+          getTooltipColor: (touchedSpot) => Colors.black45.withOpacity(0.8),
+          tooltipRoundedRadius: 1,
+        ),
+      );
 
   FlTitlesData get titlesData1 => FlTitlesData(
-    bottomTitles: AxisTitles(
-      sideTitles: bottomTitles,
-    ),
-    leftTitles: AxisTitles(
-      sideTitles: SideTitles(
-        reservedSize: 40,
-        showTitles: true,
-        interval: _delta / 2 > 0 ? _delta / 2 : 1, // Intervallo minimo di 1
-      ),
-    ),
-    rightTitles: const AxisTitles(
-      sideTitles: SideTitles(showTitles: false),
-    ),
-    topTitles: const AxisTitles(
-      sideTitles: SideTitles(showTitles: false),
-    ),
-  );
+        bottomTitles: AxisTitles(
+          sideTitles: bottomTitles,
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            reservedSize: 60,
+            showTitles: true,
+            interval: _delta / 2 > 0 ? _delta / 2 : 1, // Intervallo minimo di 1
+            getTitlesWidget: (value, meta) {
+              return SideTitleWidget(
+                axisSide: meta.axisSide,
+                space: 8.0,
+                child: Text(
+                  value.toString(), 
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 12),
+                ),
+              );
+            },
+          ),
+        ),
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+      );
 
   SideTitles get bottomTitles => SideTitles(
-    showTitles: true,
-    reservedSize: 32,
-    interval: 1, // Imposta un intervallo minimo di 1
-    getTitlesWidget: (value, meta) {
-      String text = _getLabel(value);
-      return SideTitleWidget(
-        axisSide: meta.axisSide,
-        space: 8.0,
-        child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
+        showTitles: true,
+        reservedSize: 20, // Reduced reserved size
+        interval: 1,
+        getTitlesWidget: (value, meta) {
+          String text = _getLabel(value);
+          return SideTitleWidget(
+            axisSide: meta.axisSide,
+            space: 4.0, // Reduced space
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 10, // Reduced font size
+              ),
+              overflow: TextOverflow.ellipsis, // Prevent wrapping
+            ),
+          );
+        },
       );
-    },
-  );
 
   String _getLabel(double value) {
     if (value.toInt() >= _dates.length) {
@@ -203,61 +211,62 @@ class _ChartCardState extends State<ChartCard> {
 
     final DateTime date = _dates[value.toInt()];
 
-    switch (_selected.first) {
+    switch (_selected) {
+      // Usato _selected come String
       case 'hour':
-        return DateFormat('HH:mm').format(date); // Mostra solo ore e minuti
+        return DateFormat('HH:mm').format(date);
       case 'day':
-        return DateFormat('dd/MM').format(date); // Mostra giorno e mese
+        return DateFormat('dd/MM').format(date);
       case 'min':
       default:
-        return DateFormat('dd/MM/yyyy').format(date); // Mostra la data completa nel formato 'dd/MM/yyyy'
+        return DateFormat('dd/MM/yyyy').format(date);
     }
   }
 
   List<LineChartBarData> get lineBarsData1 => [
-    lineChartBarData1_1,
-    lineChartBarData1_2,
-  ];
+        lineChartBarData1_1,
+        lineChartBarData1_2,
+      ];
 
   FlGridData get gridData => FlGridData(
-    show: true,
-    drawHorizontalLine: true,
-    drawVerticalLine: true,
-    horizontalInterval: _delta / 2 > 0 ? _delta / 2 : 1, // Assegna 1 come default se _delta / 2 è 0
-  );
+        show: true,
+        drawHorizontalLine: true,
+        drawVerticalLine: true,
+        horizontalInterval: _delta / 2 > 0 ? _delta / 2 : 1,
+      );
 
   FlBorderData get borderData => FlBorderData(
-    show: true,
-    border: const Border(
-      bottom: BorderSide(color: Colors.black, width: 0.8),
-      left: BorderSide(color: Colors.transparent),
-      right: BorderSide(color: Colors.transparent),
-      top: BorderSide(color: Colors.transparent),
-    ),
-  );
+        show: true,
+        border: const Border(
+          bottom: BorderSide(color: Colors.black, width: 0.8),
+          left: BorderSide(color: Colors.transparent),
+          right: BorderSide(color: Colors.transparent),
+          top: BorderSide(color: Colors.transparent),
+        ),
+      );
 
   LineChartBarData get lineChartBarData1_1 => LineChartBarData(
-    isCurved: true,
-    color: Colors.green,
-    barWidth: 2,
-    isStrokeCapRound: true,
-    dotData: const FlDotData(show: false),
-    belowBarData: BarAreaData(show: false),
-    spots: _listDots1,
-  );
+        isCurved: false,
+        color: Colors.green,
+        barWidth: 2,
+        isStrokeCapRound: false,
+        dotData: const FlDotData(show: true),
+        belowBarData: BarAreaData(show: false),
+        spots: _listDotsMax,
+      );
 
   LineChartBarData get lineChartBarData1_2 => LineChartBarData(
-    isCurved: true,
-    color: Colors.pink,
-    barWidth: 2,
-    isStrokeCapRound: false,
-    dotData: const FlDotData(show: false),
-    belowBarData: BarAreaData(
-      show: true,
-      color: Colors.pink.withOpacity(0),
-    ),
-    spots: _listDots2,
-  );
+        isCurved: false,
+        color: Colors.pink,
+        barWidth: 2,
+        isStrokeCapRound: false,
+        dotData: const FlDotData(show: true),
+        belowBarData: BarAreaData(
+          show: true,
+          color: Colors.pink.withOpacity(0),
+        ),
+        spots: _listDotsMin,
+      );
 }
 
 class CustomBoxForMeasure extends StatelessWidget {
@@ -280,7 +289,8 @@ class CustomBoxForMeasure extends StatelessWidget {
         Row(
           children: [
             Container(
-              margin: EdgeInsets.only(right: screenWidth * 0.02),
+              margin: EdgeInsets.only(
+                  right: screenWidth * 0.02, left: screenWidth * 0.04),
               color: color,
               constraints: BoxConstraints(
                 maxHeight: screenWidth * 0.03,
